67808011c33b2332affb36e23b696cf19eff18c1
lindexi <lindexi_gd@163.com>
2020-11-15 19:58:27 +0800

    提供 WPF 的类型的辅助方法

---
+++ src/dotnetCampus.Configurations/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("dotnetCampus.Configurations.WPFTypeConverter")]
+[assembly: InternalsVisibleTo("dotnetCampus.Configurations.Tests")]
