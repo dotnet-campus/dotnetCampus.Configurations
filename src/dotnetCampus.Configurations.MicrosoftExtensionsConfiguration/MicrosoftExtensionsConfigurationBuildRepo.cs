efc20e77e0b1fc2bee51564379e82b3fecf1923e
lindexi <lindexi_gd@163.com>
2020-11-24 17:39:09 +0800

    加入对官方配置的兼容

---
+++ src/dotnetCampus.Configurations.MicrosoftExtensionsConfiguration/MicrosoftExtensionsConfigurationBuildRepo.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using dotnetCampus.Configurations.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
+
+namespace dotnetCampus.Configurations.MicrosoftExtensionsConfiguration
+{
+    internal class MicrosoftExtensionsConfigurationBuildRepo : ConfigurationRepo
+    {
+        public MicrosoftExtensionsConfigurationBuildRepo(IConfigurationBuilder configuration)
+        {
+            configuration.Add(new ConfigurationSource(_concurrentDictionary));
+        }
+
+        public override string? GetValue(string key)
+        {
+            if (_concurrentDictionary.TryGetValue(key, out var value))
+            {
+                return value;
+            }
+
+            return null;
+        }
+
+        public override void SetValue(string key, string? value)
+        {
+            if (value is null)
+            {
+                _concurrentDictionary.TryRemove(key, out _);
+            }
+            else
+            {
+                _concurrentDictionary[key] = value;
+            }
+        }
+
+        public override void ClearValues(Predicate<string> keyFilter)
+        {
+            foreach (var key in _concurrentDictionary.Keys)
+            {
+                if (keyFilter(key))
+                {
+                    SetValue(key, null);
+                }
+            }
+        }
+
+        private readonly ConcurrentDictionary<string, string> _concurrentDictionary =
+            new ConcurrentDictionary<string, string>();
+
+        class ConfigurationSource : IConfigurationSource
+        {
+            public ConfigurationSource(ConcurrentDictionary<string, string> concurrentDictionary)
+            {
+                _concurrentDictionary = concurrentDictionary;
+            }
+
+            private readonly ConcurrentDictionary<string, string> _concurrentDictionary;
+
+            public IConfigurationProvider Build(IConfigurationBuilder builder)
+            {
+                return new MemoryConfigurationProvider(_concurrentDictionary);
+            }
+        }
+
+        class MemoryConfigurationProvider : IConfigurationProvider
+        {
+            public MemoryConfigurationProvider(ConcurrentDictionary<string, string>? concurrentDictionary = null)
+            {
+                _concurrentDictionary = concurrentDictionary ?? new ConcurrentDictionary<string, string>();
+            }
+
+            private readonly ConcurrentDictionary<string, string> _concurrentDictionary;
+
+            public bool TryGet(string key, out string value)
+            {
+                return _concurrentDictionary.TryGetValue(key, out value!);
+            }
+
+            public void Set(string key, string value)
+            {
+                _concurrentDictionary[key] = value;
+            }
+
+            public IChangeToken GetReloadToken()
+            {
+                return new ChangeToken();
+            }
+
+            class ChangeToken : IChangeToken
+            {
+                public IDisposable RegisterChangeCallback(Action<object> callback, object state) =>
+                    new EmptyDisposable();
+
+                public bool HasChanged => false;
+                public bool ActiveChangeCallbacks => false;
+
+                class EmptyDisposable : IDisposable
+                {
+                    public void Dispose()
+                    {
+                    }
+                }
+            }
+
+            public void Load()
+            {
+            }
+
+            public IEnumerable<string> GetChildKeys(IEnumerable<string> earlierKeys, string parentPath)
+            {
+                return Array.Empty<string>();
+            }
+        }
+    }
+}
---
+++ src/dotnetCampus.Configurations/Core/ConfigurationRepo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace dotnetCampus.Configurations.Core
+{
+    /// <summary>
+    /// 提供一个同步 <see cref="Configuration"/> 配置管理仓库的基类。
+    /// </summary>
+    public abstract class ConfigurationRepo : IConfigurationRepo
+    {
+        /// <inheritdoc />
+        public IAppConfigurator CreateAppConfigurator() => new ConcurrentAppConfigurator(this);
+
+        /// <inheritdoc />
+        public abstract string? GetValue(string key);
+
+        /// <inheritdoc />
+        public abstract void SetValue(string key, string? value);
+
+        /// <inheritdoc />
+        public abstract void ClearValues(Predicate<string> keyFilter);
+    }
+}
