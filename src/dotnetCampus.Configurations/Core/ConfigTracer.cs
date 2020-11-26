5eedaff48dc6d44418dc450ee39c70a6eac4b29f
walterlv <xblero@gmail.com>
2020-11-26 19:36:09 +0800

    日志输出时间

--- src/dotnetCampus.Configurations/Core/ConfigTracer.cs
+++ src/dotnetCampus.Configurations/Core/ConfigTracer.cs
@@ -1,15 +1,17 @@
 ﻿#pragma warning disable CA1810 // Initialize reference type static fields inline
 
+using System;
+using System.Diagnostics;
 using System.Linq;
 
 namespace dotnetCampus.Configurations.Core
 {
     internal class ConfigTracer
     {
-        //[Conditional("TRACETAG")]
+        [Conditional("TRACETAG")]
         internal static void Debug(string message, params string[] tags)
         {
-            System.Diagnostics.Debug.WriteLine($"{string.Join(" ", tags.Select(x => $"[{x}]"))} {message}");
+            System.Diagnostics.Debug.WriteLine($"[{DateTime.Now:O}] {string.Join(" ", tags.Select(x => $"[{x}]"))} {message}");
         }
     }
 }
