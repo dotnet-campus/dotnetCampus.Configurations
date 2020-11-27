7ce5269823f0b07d8df4e0da35fa0ded208c6d62
walterlv <xblero@gmail.com>
2020-11-27 13:10:37 +0800

    将内部仓库中已编写的单元测试都合并进来

---
+++ tests/dotnetCampus.Configurations.Tests/DefaultConfigurationTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTest.Extensions.Contracts;
+
+namespace dotnetCampus.Configurations.Tests
+{
+    [TestClass]
+    public class DefaultConfigurationTests
+    {
+        [ContractTestCase]
+        public void FromFile()
+        {
+            "从文件获取一个默认的配置，可以得到配置。".Test(() =>
+            {
+                // Arrange
+                var coin = TestUtil.GetTempFile("configs.sim.coin");
+
+                // Action
+                var configs = DefaultConfiguration.FromFile(coin.FullName);
+
+                // Assert
+                var value = configs["Foo"];
+                Assert.IsFalse(value.HasValue);
+            });
+        }
+    }
+}
