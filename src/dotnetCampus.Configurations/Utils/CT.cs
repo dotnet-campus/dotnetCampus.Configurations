d9775524a39208418c4ee5d2135699c6254c9f07
walterlv <xblero@gmail.com>
2020-11-27 20:40:54 +0800

    去掉单元测试里专用的日志输出

--- src/dotnetCampus.Configurations/Utils/CT.cs
+++ src/dotnetCampus.Configurations/Utils/CT.cs
@@ -10,27 +10,15 @@ namespace dotnetCampus.Configurations.Utils
     internal static class CT
     {
         /// <summary>
-        /// 获取或设置当前已设置的自定义输出重定向器。
-        /// </summary>
-        internal static Action<string>? OutputRedirector { get; set; }
-
-        /// <summary>
         /// 记一个日志。
         /// </summary>
         /// <param name="message">消息。</param>
         /// <param name="tags">消息标签（用于分类）。</param>
-        //[Conditional("UNITTEST")]
+        [Conditional("UNITTEST")]
         internal static void Log(string message, params string[] tags)
         {
             var text = $"[{DateTime.Now:O}] {string.Join(" ", tags.Select(x => $"[{x}]"))} {message}";
-            if (OutputRedirector is null)
-            {
-                Debug.WriteLine(text);
-            }
-            else
-            {
-                OutputRedirector(text);
-            }
+            Debug.WriteLine(text);
         }
     }
 }
--- tests/dotnetCampus.Configurations.Tests/ConfigurationStringTests.cs
+++ tests/dotnetCampus.Configurations.Tests/ConfigurationStringTests.cs
@@ -7,17 +7,6 @@ namespace dotnetCampus.Configurations.Tests
     [TestClass]
     public class ConfigurationStringTests
     {
-        /// <summary>
-        /// 用于存储提供给单元测试的信息。
-        /// </summary>
-        public TestContext TestContext { get; set; }
-
-        [TestInitialize]
-        public void Initialize()
-        {
-            CT.OutputRedirector = text => TestContext.WriteLine(text);
-        }
-
         [ContractTestCase]
         public void Convert()
         {
